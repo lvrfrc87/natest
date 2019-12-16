e4b68fdb65a13f25f721d5dadc5040fcc46d8fda
federico.olivieri <federico.olivieri@ticketmaster.co.uk>
2019-12-16 21:54:53 +0100

    Ver 0.1

--- tmtest.py
+++ tmtest.py
@@ -1,47 +1,74 @@
 #!/usr/bin/env python3
 import os
+from termcolor import colored
 
 class tmTest():
 
-    def __init__(self,input, want, have):
-        self.input = input
-        self.want = want
-        self.have = have
+    def __init__(self):
+        pass
+
+    def read(self, input):
+        '''read an input example (i.e. ssh show command) and return
+        a list of splitlines() that can be use along the code.
+        The input can be can be a str() varibale as well as a file
+        (ideally under ./fixtures folder).'''
+
+        input_list = list()
+
+        for line in input:
+            input_list.append(line)
+
+        return input_list
 
 
-    def read(self):
-        fixture_list = list()
-        open_fixture = open('./fixtures/{}'.format(self.input), 'r')
-
-        for line in open_fixture:
-            fixture_list.append(line)
-
-        return fixture_list
-
-
-    def result(self):
+    def result(self, want, have):
+        '''WANT: pass the file name (under ./fitures) containing the desired output.
+                 The file is converted in a list splitted by lines.
+           HAVE: pass the return of your code. This can be a list of splitted lines
+                 or a string (that will converted in a list of splitted lines)
+        '''
 
         want_list = list()
         have_list = list()
 
-        read_want = open('./fixtures/{}'.format(self.want), 'r')
+        read_want = open('./fixtures/{}'.format(want), 'r')
 
         for line in read_want:
             want_list.append(line)
 
-        if type(self.have) is 'list':
-            have_list = self.have
+        if type(have) is 'list':
+            have_list = have
         else:
-            for line in self.have:
+            for line in have:
                 have_list.append(line)
 
         want_diff = [diff for diff in want_list if diff not in have_list]
         have_diff = [diff for diff in have_list if diff not in want_list]
-        print(want_diff,have_diff)
+
+        return_want = list()
+        return_have = list()
+
+        if want_diff:
+            color_w = 'yellow'
+            for line in want_diff:
+                return_want.append('+ {}'.format(str(line)))
+        else:
+            color_w = 'green'
+
+        if have_diff:
+            color_h = 'red'
+            for line in have_diff:
+                return_have.append('- {}'.format(str(line)))
+        else:
+            color_h = 'green'
+
+
+        return colored(return_want, color_w) + '\n' + colored(return_have, color_h)
 
 
 
 if __name__ == '__main__':
-    a = tmTest('input', 'want', 'have')
-        # print(a.read())
-    print(a.result())
+    input = open('./fixtures/ssh_input', 'r')
+    a = tmTest()
+    have = open('./fixtures/have', 'r')
+    print(a.result('want', have))
